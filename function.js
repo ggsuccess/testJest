8f73b25aefe5fe4ed629aebede1f3440985b1c15
ggsuccess <ggsuccess@naver.com>
2019-12-20 18:59:52 +0900

    div

--- function.js
+++ function.js
@@ -1,7 +1,8 @@
 const functions = {
   add: (num1, num2) => num1 + num2,
   sub: (num1, num2) => num1 - num2,
-  mul: (num1, num2) => num1 * num2
+  mul: (num1, num2) => num1 * num2,
+  div: (num1, num2) => num1 / num2
 };
 
 module.exports = functions;
--- function.test.js
+++ function.test.js
@@ -3,3 +3,4 @@ const functions = require('./function');
 test('add 2+4 to equal 6', () => expect(functions.add(2, 4)).toBe(6));
 test('sub 12-5 to equal 7', () => expect(functions.sub(12, 5)).toBe(7));
 test('mul 6x5 to equal 30', () => expect(functions.mul(6, 5)).toBe(30));
+test('div 10/2 to equal 5', () => expect(functions.div(10, 2)).toBe(5));
